b44881820e699164ffe9b921171a6550e40fc1fa
stsiapan <shtefsv@gmail.com>
2024-03-08 23:13:46 +0300

    Defined project structure, implemented application layers, added Maven and Spring Boot dependencies

---
+++ src/main/java/org/garden/com/GardenApp.java
@@ -0,0 +1,12 @@
+package org.garden.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GardenApp.class, args);
+    }
+}
