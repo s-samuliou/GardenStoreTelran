071ba8453ca79229b10f0db90bb4be392ee814dc
sparklersoft <sparklersoft@gmail.com>
2024-03-10 00:02:50 +0100

    Order class and order statuses enums were added

---
+++ src/main/java/org/garden/com/entity/Order.java
@@ -0,0 +1,53 @@
+package org.garden.com.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
+
+    @Column(name = "user_id")
+    private int userId;
+
+    @NotNull(message = "Created at timestamp is required")
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @NotBlank(message = "Delivery address is required")
+    @Length(max = 500, message = "Address must be at least 10 characters and not nigher than 500")
+    @Column(name = "delivery_address")
+    private String deliveryAddress;
+
+    @NotNull(message = "Contact phone is required")
+    @Column(name = "contact_phone")
+    private String contactPhone;
+
+    @NotBlank(message = "Delivery method is required")
+    @Length(max = 255, message = "Delivery method must be less than 255 characters")
+    @Column(name = "delivery_method")
+    private String deliveryMethod;
+
+    private Enum status;
+
+    @NotNull(message = "Updated at timestamp is required")
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+
+}
+
---
+++ src/main/java/org/garden/com/enums/OrderStatus.java
@@ -0,0 +1,14 @@
+package org.garden.com.enums;
+
+public enum OrderStatus {
+    CREATED,
+    UPDATED,
+    PAID,
+    PROCESSING,
+    CONFIRMED,
+    SHIPPED,
+    DELIVERED,
+    CANCELED,
+    RETURNED
+
+}
