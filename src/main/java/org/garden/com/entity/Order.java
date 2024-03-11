d5c25c99e3b3899a26bfeede7ad3f2afa5e4a81a
sparklersoft <sparklersoft@gmail.com>
2024-03-11 09:05:51 +0100

    GARD-5. In Order_class OrderStatus changed from enums and some unnecessary spaced were deleted.

--- src/main/java/org/garden/com/entity/Order.java
+++ src/main/java/org/garden/com/entity/Order.java
@@ -1,12 +1,10 @@
 package org.garden.com.entity;
 
-
 import jakarta.persistence.*;
-import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import org.garden.com.enums.OrderStatus;
 import org.hibernate.validator.constraints.Length;
 
 import java.time.LocalDateTime;
@@ -22,7 +20,7 @@ public class Order {
     private Long id;
 
     @Column(name = "user_id")
-    private int userId;
+    private long userId;
 
     @NotNull(message = "Created at timestamp is required")
     @Column(name = "created_at")
@@ -42,12 +40,10 @@ public class Order {
     @Column(name = "delivery_method")
     private String deliveryMethod;
 
-    private Enum status;
+    private OrderStatus status;
 
     @NotNull(message = "Updated at timestamp is required")
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
-
-
 }
 
--- src/main/java/org/garden/com/enums/OrderStatus.java
+++ src/main/java/org/garden/com/enums/OrderStatus.java
@@ -10,5 +10,4 @@ public enum OrderStatus {
     DELIVERED,
     CANCELED,
     RETURNED
-
 }
