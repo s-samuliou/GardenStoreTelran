5a2ea531853d6623c82e36de4b94ddc10e184308
Balagurchyk <balagurchyk@gmail.com>
2024-03-17 11:39:55 +0100

    add EditUserDto

---
+++ src/main/java/org/garden/com/dto/EditUserDto.java
@@ -0,0 +1,28 @@
+package org.garden.com.dto;
+
+public class EditUserDto {
+
+    private String name;
+    private String phoneNumber;
+
+    public EditUserDto(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
