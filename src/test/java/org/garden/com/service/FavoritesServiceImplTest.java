016eb46616259871bd627c3b39f0a341008874ec
stsiapan <shtefsv@gmail.com>
2024-03-26 13:30:24 +0300

    Added tests for FavoritesServiceImpl

---
+++ src/test/java/org/garden/com/service/FavoritesServiceImplTest.java
@@ -0,0 +1,98 @@
+package org.garden.com.service;
+
+import jakarta.validation.Validator;
+import org.garden.com.entity.Favorites;
+import org.garden.com.entity.Product;
+import org.garden.com.entity.User;
+import org.garden.com.exceptions.FavoriteNotFoundException;
+import org.garden.com.repository.FavoritesJpaRepository;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.*;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class FavoritesServiceImplTest {
+
+    @InjectMocks
+    private FavoritesServiceImpl favoritesService;
+
+    @Mock
+    private FavoritesJpaRepository repository;
+
+    @Mock
+    private Validator validator;
+
+    public FavoritesServiceImplTest() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void addFavoriteProduct_ValidFavorite_ReturnsCreatedFavorite() {
+        User user = new User();
+        user.setId(1L);
+
+        Product product = new Product();
+        product.setId(1L);
+
+        Favorites favorite = new Favorites();
+        favorite.setUser(user);
+        favorite.setProduct(product);
+
+        when(repository.save(favorite)).thenReturn(favorite);
+        Favorites createdFavorite = favoritesService.addFavoriteProduct(favorite);
+
+        assertNotNull(createdFavorite);
+        assertEquals(favorite, createdFavorite);
+        verify(repository, times(1)).save(favorite);
+    }
+
+    @Test
+    public void getAllFavorites_ReturnsListOfFavorites() {
+        List<Favorites> favoriteList = new ArrayList<>();
+        favoriteList.add(new Favorites());
+        favoriteList.add(new Favorites());
+
+        when(repository.findAll()).thenReturn(favoriteList);
+
+        List<Favorites> retrievedFavorites = favoritesService.getAllFavorites();
+
+        assertNotNull(retrievedFavorites);
+        assertEquals(2, retrievedFavorites.size());
+        verify(repository, times(1)).findAll();
+    }
+
+    @Test
+    public void deleteFavorite_ExistingFavorite_ReturnsOkResponse() {
+        long id = 1L;
+        when(repository.existsById(id)).thenReturn(true);
+
+        ResponseEntity<Void> result = favoritesService.deleteFavorite(id);
+
+        assertNotNull(result);
+        assertEquals(ResponseEntity.status(HttpStatus.OK).build(), result);
+        verify(repository, times(1)).existsById(id);
+        verify(repository, times(1)).deleteById(id);
+    }
+
+    @Test
+    public void deleteFavorite_NonExistingFavorite_ThrowsFavoriteNotFoundException() {
+        long id = 1L;
+        when(repository.existsById(id)).thenReturn(false);
+
+        assertThrows(FavoriteNotFoundException.class, () -> favoritesService.deleteFavorite(id));
+        verify(repository, times(1)).existsById(id);
+        verify(repository, never()).deleteById(id);
+    }
+}
